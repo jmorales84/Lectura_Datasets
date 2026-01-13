fb9087f4740118530b9452999104e8f341463c9d
jmorales84 <jessicavegamorales8@gmail.com>
2026-01-13 08:25:49 -0600

    Actualizacion

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -8,4 +8,4 @@
 
 // === TUNNEL (producción / demo) ===
 // const API_BASE_URL = "https://t-71r6n9of.tunn.dev";
-const API_BASE_URL = "https://sur-middle-zones-chat.trycloudflare.com"
+const API_BASE_URL = "https://scratch-makes-shown-implies.trycloudflare.com"
